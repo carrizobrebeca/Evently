39654e98e9244c1220f3c2d4812afec7536457b2
carrizobrebeca <carrizob.rebeca@gmail.com>
2025-05-25 13:36:09 -0300

    actualizado

--- src/Components/Pages/Event.jsx
+++ src/Components/Pages/Event.jsx
@@ -1,86 +1,84 @@
-
 import React, { useState } from "react";
 import SideBarL from "./SideBarL";
 import HeaderEvent from "./HeaderEvent";
+import Comment from "./Comment";
+import PostEvent from "./PostEvent";
 
 const Event = () => {
   const [activeTab, setActiveTab] = useState("publicaciones");
 
   const buttons = [
-    { id: "publicaciones", label: "Publicaciones", icon:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
-    </svg> },
-    { id: "participantes", label: "Participantes", icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-    </svg> },
-   
+    {
+      id: "publicaciones",
+      label: "Publicaciones",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
+          />
+        </svg>
+      ),
+    },
+    {
+      id: "participantes",
+      label: "Participantes",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+          />
+        </svg>
+      ),
+    },
   ];
   return (
     <div className="bg-gray-200 grid lg:grid-cols-5 xl:grid-cols-6 min-h-screen bg-gray-200">
       <div className="lg:col-span-1">
         <SideBarL />
       </div>
-      <div className="bg-gray-100 lg:col-span-1">
-
-      </div>
+      <div className="bg-gray-100 lg:col-span-1"></div>
       <main className="lg:col-span-2 xl:col-span-4 bg-gray-100 p-8 h-auto overflow">
-
         <HeaderEvent />
         <div className="border-t-4 border-gray-300 flex justify-around">
-        {buttons.map(({ id, label, icon }) => (
-        <button
-          key={id}
-          onClick={() => setActiveTab(id)}
-          className={`pt-6 flex items-center justify-center gap-2 border-t-4 ${
-            activeTab === id ? "border-gray-300" : "border-gray-100"
-          }`}
-        >
-          <span>{icon}</span>
-          {label}
-        </button>
-      ))}
+          {buttons.map(({ id, label, icon }) => (
+            <button
+              key={id}
+              onClick={() => setActiveTab(id)}
+              className={`pt-6 flex items-center justify-center gap-2 border-t-4 ${
+                activeTab === id ? "border-gray-300" : "border-gray-100"
+              }`}
+            >
+              <span>{icon}</span>
+              {label}
+            </button>
+          ))}
         </div>
-        <section className="mt-10 mb-10">
-          <div className="bg-gray-200 p-8 rounded-xl text-gray-500 text-lg">
-            <h2>Publicacion</h2>
-          </div>
-        </section>
-        <section className="mt-10 mb-10">
-          <div className="bg-gray-200 p-8 rounded-xl text-gray-500 text-lg">
-            <h2>Publicacion</h2>
-          </div>
-        </section>
-        <section className="mt-10 mb-10">
-          <div className="bg-gray-200 p-8 rounded-xl text-gray-500 text-lg">
-            <h2>Publicacion</h2>
-          </div>
-        </section>
-        <section className="mt-10 mb-10">
-          <div className="bg-gray-200 p-8 rounded-xl text-gray-500 text-lg">
-            <h2>Publicacion</h2>
-          </div>
-        </section>
-        <section className="mt-10 mb-10">
-          <div className="bg-gray-200 p-8 rounded-xl text-gray-500 text-lg">
-            <h2>Publicacion</h2>
-          </div>
-        </section>
-        <section className="mt-10 mb-10">
-          <div className="bg-gray-200 p-8 rounded-xl text-gray-500 text-lg">
-            <h2>Publicacion</h2>
-          </div>
-        </section>
-        <section className="mt-10 mb-10">
-          <div className="bg-gray-200 p-8 rounded-xl text-gray-500 text-lg">
-            <h2>Publicacion</h2>
-          </div>
-        </section>
 
+        <Comment />
 
+        <section className="mt-10 mb-10">
+          <PostEvent />
+        </section>
       </main>
-      <div className="bg-gray-100 lg:col-span-1">
-
-      </div>
+      <div className="bg-gray-100 lg:col-span-1"></div>
     </div>
   );
 };
---
+++ src/Components/Pages/PostEvent.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import post from "../../assets/post.PNG";
+const PostEvent = () => {
+  return (
+    <div className="flex flex-row md:flex-row items-center justify-between gap-4  pt-6 pb-6">
+      <form className="w-full p-10 bg-gray-200 rounded-3xl">
+        <div className="flex justify-between p-4">
+          <img
+            src="https://w7.pngwing.com/pngs/857/213/png-transparent-man-avatar-user-business-avatar-icon.png"
+            className="w-10 h-10 object-cover rounded-full "
+          />
+          <h2 className="text-gray-800 text-bold">Nombre</h2>
+          <h2 className="text-gray-600 text-bold">Usuario</h2>
+          <h2 className="text-gray-600 text-bold">Fecha</h2>
+
+       
+        </div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex numquam voluptatum corrupti ut quibusdam amet laboriosam voluptatem aspernatur, deleniti quos sed delectus architecto officiis laborum, perspiciatis repellat dolorum facere iste!</p>
+        
+        <div className="flex justify-between p-4">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z"
+              />
+            </svg>
+          </button>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z"
+              />
+            </svg>
+          </button>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3"
+              />
+            </svg>
+          </button>
+          <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+</svg>
+</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PostEvent;
